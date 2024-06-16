3a827afbfd2eccf01ba7030dee7069433428974e
zhishixiang <zhishixiang@outlook.com>
2024-06-16 20:20:54 +0800

    修改api地址

--- src/exam_webserver.rs
+++ src/exam_webserver.rs
@@ -3,9 +3,9 @@ use actix_files::NamedFile;
 use actix_web::{get, post, Result, web, App, HttpResponse, HttpServer, Responder, HttpRequest};
 
 #[get("/{test_id}")]
-async fn index(path: web::Path<(String)>) -> impl Responder {
+async fn index(path: web::Path<(String)>) -> Result<NamedFile> {
     let test_id = path.into_inner();
-    HttpResponse::Ok().body(format!(" {test_id}"))
+    Ok(NamedFile::open(PathBuf::from("templates/exam.html"))?)
 }
 async fn resources(req: HttpRequest) -> Result<NamedFile> {
     let mut path:PathBuf = PathBuf::from("resources/");
@@ -13,7 +13,12 @@ async fn resources(req: HttpRequest) -> Result<NamedFile> {
     path.push(filename);
     Ok(NamedFile::open(path)?)
 }
-
+async fn get_test(req: HttpRequest) -> Result<NamedFile> {
+    let mut path:PathBuf = PathBuf::from("tests/");
+    let filename:String = req.match_info().query("filename").parse().unwrap();
+    path.push(filename);
+    Ok(NamedFile::open(path)?)
+}
 pub fn new_actix_server(){
     let sys = actix_rt::System::new();
     sys.block_on(async {
@@ -21,6 +26,10 @@ pub fn new_actix_server(){
             App::new()
                 .service(index)
                 .route("/resources/{filename:.*}",web::get().to(resources))
+                .service(
+                    web::scope("/api")
+                        .route("/get_test/{filename:.*}",web::get().to(get_test))
+                )
         })
             .bind("127.0.0.1:8081")
             .expect("HTTP服务无法绑定端口")
