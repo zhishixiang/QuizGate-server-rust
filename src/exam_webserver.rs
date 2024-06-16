610441c41453117cd83bdb1e84645fc938a418b7
zhishixiang <zhishixiang@outlook.com>
2024-06-16 21:25:55 +0800

    评分

--- src/exam_webserver.rs
+++ src/exam_webserver.rs
@@ -5,7 +5,11 @@ use std::path::PathBuf;
 use actix_files::NamedFile;
 use actix_web::{App, get, HttpRequest, HttpResponse, HttpServer, Result, web};
 use std::path::Path;
+use actix_web::web::to;
+use serde::Deserialize;
 use serde_json::{json, Value};
+use crate::structs::submit::{SubmitRequest, SubmitResponse};
+
 
 #[get("/{test_id}")]
 async fn index(path: web::Path<(String)>) -> Result<NamedFile> {
@@ -55,6 +59,58 @@ async fn get_test(req: HttpRequest) -> HttpResponse {
         "is_server_online": true
     }))
 }
+async fn submit(req_body: web::Json<SubmitRequest>) -> HttpResponse{
+    let answer = &req_body.answer;
+    let player_id = &req_body.player_id;
+    let test_id = &req_body.paper_id;
+    let file_path = format!("tests/{}.json", test_id);
+    let mut score = 0;
+    let mut paper_info: Value = json!({});
+    if Path::new(&file_path).exists() {
+        let mut file = match File::open(&file_path) {
+            Ok(file) => file,
+            Err(_) => return HttpResponse::InternalServerError().json(json!({"code": 500})),
+        };
+
+        let mut contents = String::new();
+        if let Err(_) = file.read_to_string(&mut contents) {
+            return HttpResponse::InternalServerError().json(json!({"code": 500}));
+        }
+
+        paper_info = match serde_json::from_str(&contents) {
+            Ok(json) => json,
+            Err(_) => return HttpResponse::InternalServerError().json(json!({"code": 500})),
+        };
+
+        let questions = paper_info["questions"].as_array().unwrap();
+        for (i, question) in questions.iter().enumerate() {
+            if question["type"] == "multiple" {
+                if answer[i] == question["correct"] {
+                    score += question["score"][1].as_i64().unwrap();
+                } else if answer[i].as_i64() < question["correct"].as_i64() && !answer[i].is_null() {
+                    score += question["score"][0].as_i64().unwrap();
+                }
+            } else if question["type"] == "radio" {
+                if answer[i] == question["correct"] {
+                    score += question["score"].as_i64().unwrap();
+                }
+            }
+        }
+    } else {
+        return HttpResponse::NotFound().json(json!({"code": 404}));
+    }
+    if score > paper_info["pass"].as_i64().unwrap() {
+        return HttpResponse::Ok().json(SubmitResponse {
+            score,
+            pass: true,
+        });
+    }else {
+        return HttpResponse::Ok().json(SubmitResponse {
+            score,
+            pass: false,
+        });
+    }
+}
 pub fn new_actix_server(){
     let sys = actix_rt::System::new();
     sys.block_on(async {
@@ -65,6 +121,7 @@ pub fn new_actix_server(){
                 .service(
                     web::scope("/api")
                         .route("/get_test/{filename:.*}",web::get().to(get_test))
+                        .route("/submit",web::post().to(submit))
                 )
         })
             .bind("127.0.0.1:8081")
---
+++ src/structs/submit.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+// 此处存放对于提交的试卷进行解析和响应的结构体
+#[derive(Deserialize)]
+pub struct SubmitRequest {
+    pub(crate) answer: Vec<Value>,
+    pub(crate) player_id: String,
+    pub(crate) paper_id: String,
+}
+#[derive(Serialize)]
+pub struct SubmitResponse {
+    pub(crate) score: i64,
+    pub(crate) pass: bool,
+}
