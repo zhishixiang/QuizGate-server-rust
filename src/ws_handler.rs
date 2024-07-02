32d373bf71b44da311f11aa56df0e93d45a56972
zhishixiang <zhishixiang@outlook.com>
2024-07-02 21:55:15 +0800

    将消息处理逻辑分离为建立前和建立后

--- src/ws_handler.rs
+++ src/ws_handler.rs
@@ -4,7 +4,7 @@ use tokio::sync::mpsc;
 use log::error;
 use sqlx::{pool::Pool, sqlite::Sqlite};
 use tungstenite::Error::{ConnectionClosed, Io};
-use tungstenite::Message;
+use tungstenite::{Error, Message};
 use crate::error::{ConnectionClosedError, NoSuchKeyError};
 use crate::{Client, ClientList, WsStream};
 use crate::structs::respond::Respond;
@@ -16,90 +16,101 @@ pub async fn ws_handler(ws_stream: WsStream, sql_pool: Arc<Pool<Sqlite>>, mut cl
     //状态码定义：0为初始化，1为连接成功，只有为0时才进行验证流程
     let mut status_code: i8 = 0;
     let mut client_key = String::new();
-    // 读取消息
-    while let Some(msg) = read.next().await {
-        match msg {
-            Ok(msg) => match msg {
-                Message::Text(msg) => {
-                    if status_code == 0 {
-                        // 获取密钥
-                        let key = msg.to_string();
-                        // 验证密钥是否存在
-                        let result: Result<Option<(String, )>, sqlx::Error> = sqlx::query_as("SELECT name FROM server_info WHERE key = ?")
-                            .bind(&key)
-                            .fetch_optional(&*sql_pool)
-                            .await;
-                        match result {
-                            Ok(Some(row)) => {
-                                println!("客户端{}上线", row.0);
-                                status_code = 1;
-                                // 获取锁
-                                let mut client_list = client_list.lock().await;
-                                // 将客户端信息推送至客户端列表
-                                client_list.push(Client { client_key: key.clone(), client_handler: tx.clone() });
-                                client_key = key;
-                                // 释放锁
-                                drop(client_list);
-                                // 发送响应数据
-                                let respond = Respond { code: 0, msg: row.0 };
-                                let _ = write.send(Message::Binary(serde_json::to_vec(&respond).unwrap())).await;
-                                loop {
-                                    let add_player_respond = Respond { code: 2, msg: rx.recv().await.unwrap() };
-                                    let _ = write.send(Message::Binary(serde_json::to_vec(&add_player_respond).unwrap())).await;
-                                    println!("1");
+    // 初始化链接
+    while status_code == 0 {
+        if let Some(msg) = read.next().await {
+            match msg {
+                Ok(Message::Text(msg)) =>  {
+                        if status_code == 0 {
+                            // 获取密钥
+                            let key = msg.to_string();
+                            // 验证密钥是否存在
+                            let result: Result<Option<(String, )>, sqlx::Error> = sqlx::query_as("SELECT name FROM server_info WHERE key = ?")
+                                .bind(&key)
+                                .fetch_optional(&*sql_pool)
+                                .await;
+                            match result {
+                                Ok(Some(row)) => {
+                                    println!("客户端{}上线", row.0);
+                                    status_code = 1;
+                                    // 获取锁
+                                    let mut client_list = client_list.lock().await;
+                                    // 将客户端信息推送至客户端列表
+                                    client_list.push(Client { client_key: key.clone(), client_handler: tx.clone() });
+                                    client_key = key;
+                                    // 释放锁
+                                    drop(client_list);
+                                    // 发送响应数据
+                                    let respond = Respond { code: 0, msg: row.0 };
+                                    let _ = write.send(Message::Binary(serde_json::to_vec(&respond).unwrap())).await;
+                                    loop {
+                                        let add_player_respond = Respond { code: 2, msg: rx.recv().await.unwrap() };
+                                        let _ = write.send(Message::Binary(serde_json::to_vec(&add_player_respond).unwrap())).await;
+                                        println!("1");
+                                    }
                                 }
-                            }
-                            Ok(None) => {
-                                eprintln!("客户端发送了无效的key，断开链接");
-                                let _ = write.send(Message::Close(None)).await;
-                                return Err(NoSuchKeyError.into());
-                            }
-                            Err(e) => {
-                                error!("Failed to execute query: {}", e);
-                                let _ = write.send(Message::Close(None)).await;
-                                return Err(ConnectionClosedError.into());
+                                Ok(None) => {
+                                    eprintln!("客户端发送了无效的key，断开链接");
+                                    let _ = write.send(Message::Close(None)).await;
+                                    return Err(NoSuchKeyError.into());
+                                }
+                                Err(e) => {
+                                    error!("Failed to execute query: {}", e);
+                                    let _ = write.send(Message::Close(None)).await;
+                                    return Err(ConnectionClosedError.into());
+                                }
                             }
                         }
                     }
-                }
-                Message::Ping(_) | Message::Pong(_) => {
-                    // Ping and Pong messages can be handled here
-                }
-                Message::Close(_) => {
-                    println!("Client closed the connection");
-                    if status_code == 1 {
-                        remove_client(&mut client_list, client_key).await;
+                    Message::Ping(_) | Message::Pong(_) => {
+                        // Ping and Pong messages can be handled here
                     }
-                    break;
+                    Message::Close(_) => {
+                        println!("Client closed the connection");
+                        if status_code == 1 {
+                            remove_client(&mut client_list, client_key).await;
+                        }
+                        break;
+                    }
+            }
+        }
+        while status_code == 1 {
+            tokio::select! {
+            Some(msg) = read.next() => {
+                match msg {
+                    Ok(Message::Close(_)) => {
+                        println!("Client closed the connection");
+                        remove_client(&mut client_list, client_key.clone()).await;
+                        break;
+                    }
+                    Err(e) => match e {
+                        ConnectionClosed => {
+                            println!("客户端断开链接");
+                            remove_client(&mut client_list, client_key.clone()).await;
+                            break;
+                        }
+                        Io(ref err) if err.kind() == std::io::ErrorKind::ConnectionReset => {
+                            println!("客户端异常退出");
+                            remove_client(&mut client_list, client_key.clone()).await;
+                            break;
+                        }
+                        _ => {
+                            println!("Other error: {}", e);
+                            remove_client(&mut client_list, client_key.clone()).await;
+                            break;
+                        }
+                    },
+                    _ => {}
                 }
-                _ => {}
             },
-            Err(e) => match e {
-                ConnectionClosed => {
-                    println!("客户端断开链接");
-                    if status_code == 1 {
-                        remove_client(&mut client_list, client_key).await;
-                    }
-                    break;
-                }
-                Io(ref err) if err.kind() == std::io::ErrorKind::ConnectionReset => {
-                    println!("客户端异常退出");
-                    if status_code == 1 {
-                        remove_client(&mut client_list, client_key).await;
-                    }
-                    break;
-                }
-                _ => {
-                    println!("Other error: {}", e);
-                    if status_code == 1 {
-                        remove_client(&mut client_list, client_key).await;
-                    }
-                    break;
-                }
-            },
+            Some(msg) = rx.recv() => {
+                let add_player_respond = Respond { code: 2, msg };
+                let _ = write.send(Message::Binary(serde_json::to_vec(&add_player_respond).unwrap())).await;
+                println!("1");
+            }
+        }
         }
     }
-
     Ok(())
 }
 
