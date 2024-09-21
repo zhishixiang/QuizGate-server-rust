412106932140fe110a8c74c995bef79ddca5b9d3
zhishixiang <zhishixiang@outlook.com>
2024-09-21 12:20:39 +0800

    新增检测问卷是否存在的方法

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::path::Path;
 
 pub fn read_file(file_path: &String) -> actix_web::Result<File, std::io::Error> {
     let file = match File::open(&file_path) {
@@ -8,4 +9,10 @@ pub fn read_file(file_path: &String) -> actix_web::Result<File, std::io::Error> 
         },
     };
     Ok(file)
+}
+
+// 检测tests目录下是否有对应问卷
+pub fn is_test_id_exist(test_id: u32) -> bool {
+    let file_path = format!("tests/{}.json", test_id);
+    Path::new(&file_path).exists()
 }
